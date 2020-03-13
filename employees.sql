7b93e659e41a568189684a29bc41cddf4e72da08
ryan magill <R7NO@MacBook-Pro.lan1>
2020-03-13 01:11:59 -0500

    Finished the project

--- employees.sql
+++ employees.sql
@@ -6,24 +6,27 @@ CREATE DATABASE employees_db;
 -- Makes it so all of the following code will affect animals_db --
 USE employees_db;
 
--- Creates the table "people" within animals_db --
-CREATE TABLE employee (
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-);
-
 CREATE TABLE roles(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
-CREATE TABLE department(
-    id INT PRIMARY KEY,
+-- Creates the table "people" within animals_db --
+CREATE TABLE employees(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE departments(
+    id int PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30)
 );
 
+INSERT INTO roles(title, salary, department_id) VALUES ("Manager", 100000, 1); 
+INSERT INTO roles(title, salary, department_id) VALUES ("Employee", 90000, 1); 
--- server.js
+++ server.js
@@ -1,19 +1,242 @@
 // Dependencies
-var express = require("express");
+//var express = require("express");
+var mysql = require("mysql");
+
+const inquirer = require("inquirer");
+
 
 // Sets up the Express App
-let app = express();
-let PORT = process.env.PORT || 3010;
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "dennis97",
+    database: "employees_db"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    runApp();
+  });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('./public'));
+function runApp(){
 
-require("./routes/apiRoutes")(app);
+    inquirer.prompt([
+
+        {
+            type:"list",
+            name: "WTD",
+            message: "What would you like to do?",
+            choices:[
+                "Add departments, roles, or employees",
+                "View departments, roles, or employees",
+                "Update employee roles"
+            ]
+        },
+    ]).then((answer)=>{
+        if(answer.WTD === "Add departments, roles, or employees"){
+            addDRE();
+        }
+        if(answer.WTD === "View departments, roles, or employees"){
+            viewDRE();
+        }
+        if(answer.WTD === "Update employee roles"){
+            updateRoles();
+        }
+    })
+}
+
+function addDRE(){
+    inquirer.prompt([
+
+        {
+            type:"list",
+            name: "addDRE",
+            message: "Choose one",
+            choices:[
+                "Add department",
+                "Add role",
+                "Add employee",
+                "go back"
+            ]
+        },
+    ]).then((answer)=>{
+        if(answer.addDRE === "Add department"){
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name:"dName",
+                    message: "What is the name of the department?"
+                }
+            ]).then((answer)=>{
+                var query = connection.query(
+                    "INSERT INTO departments SET ?",
+                    {
+                        name: answer.dName
+                    },
+                    function(err, res) {
+                        if (err) throw err;
+                        
+                    }
+                )
+                runApp();
+            })
+        }
+
+        if(answer.addDRE === "Add role"){
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name:"rTitle",
+                    message: "What is the title of the role?"
+                },
+                {
+                    type:"input",
+                    name:"rSalary",
+                    message: "What is the salary of the role?"
+                },
+                {
+                    type:"input",
+                    name:"rDepID",
+                    message: "What is the department # of the role?"
+                }
+            ]).then((answer)=>{
+                var query = connection.query(
+                    "INSERT INTO roles SET ?",
+                    {
+                        title: answer.rTitle,
+                        salary: answer.rSalary,
+                        department_id: answer.rDepID
+                    },
+                    function(err, res) {
+                        if (err) throw err;
+                        
+                    }
+                )
+                runApp();
+            })
+            
+        }
+        if(answer.addDRE === "Add employee"){
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name:"eFName",
+                    message: "What is the first name of your employee?"
+                },
+                {
+                    type:"input",
+                    name:"eLName",
+                    message: "What is the last name of your employee?"
+                },
+                {
+                    type:"input",
+                    name:"roleID",
+                    message: "What is your employees role?"
+                },
+                {
+                    type:"input",
+                    name:"eManagerID",
+                    message: "What is your employees managers id?"
+                }
+            ]).then((answer)=>{
+                var query = connection.query(
+                    "INSERT INTO employees SET ?",
+                    {
+                        first_name: answer.eFName,
+                        last_name: answer.eLName,
+                        role_id: answer.roleID,
+                        manager_id: answer.eManagerID
+                    },
+                    function(err, res) {
+                        if (err) throw err;
+                        
+                    }
+                )
+                runApp();
+            })
+        }
+        if(answer.addDRE === "go back"){
+            runApp();
+        }
+    })
+}
+
+function viewDRE(){
+    inquirer.prompt([
+
+        {
+            type:"list",
+            name: "viewDRE",
+            message: "Choose one",
+            choices:[
+                "View departments",
+                "View roles",
+                "View employees",
+                "go back"
+            ]
+        },
+    ]).then((answer)=>{
+        if(answer.viewDRE === "View departments"){
+            connection.query("SELECT * FROM departments", function(err, res) {
+                if (err) throw err;
+                // Log all results of the SELECT statement
+                console.log(res);
+                connection.end();
+            })
+            runApp();
+        }   
+        if(answer.viewDRE === "View roles"){
+            connection.query("SELECT * FROM roles", function(err, res) {
+                if (err) throw err;
+                // Log all results of the SELECT statement
+                console.log(res);
+                connection.end();
+            })
+            runApp();
+        } 
+        if(answer.viewDRE === "View employees"){
+            connection.query("SELECT * FROM employees", function(err, res) {
+                if (err) throw err;
+                // Log all results of the SELECT statement
+                console.log(res);
+                connection.end();
+            })
+            runApp();
+        } 
+        if(answer.viewDRE === "go back"){
+            runApp();
+        }
+    })    
+}
+
+function updateRoles(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name: "empID",
+            message: "Which employee would you like to update?(Enter ID #)"
+        }
+        
+    ]).then((answer)=>{
+        connection.query( 
+            "SELECT employees.role_id, roles.id, roles.title FROM employees INNER JOIN roles ON employees.role_id=roles.id", (err,res)=>{
+                if (err) throw err;
+                // Log all results of the SELECT statement
+                console.log(res);
+                connection.end();
+            }
+            
+        )
 
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+    })
+}
+
